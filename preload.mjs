14755359bf6d4c3e58f0bc4e8cf467c47d4759c5
Spatulox <M78stormtrooper@laposte.net>
2023-12-23 14:32:37 +0100

    update preload.js

--- preload.mjs
+++ preload.mjs
@@ -1,7 +1,8 @@
 import { loadPage } from "./src/modules/loadPages.js";
+import { log } from "./src/modules/globalFunction.js";
 
 document.addEventListener("DOMContentLoaded", (event) => {
-    
+    log('Main Page loaded')
     loadPage('dashboard');
 
 });
