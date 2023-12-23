b0cedd6ae1970e08ac7a1e9c3e990a163162507c
Spatulox <M78stormtrooper@laposte.net>
2023-12-23 14:43:24 +0100

    using log in preload instead of main.js

--- preload.mjs
+++ preload.mjs
@@ -1,6 +1,8 @@
 import { loadPage } from "./src/modules/loadPages.js";
 import { log } from "./src/modules/globalFunction.js";
 
+log('---------------')
+log('Electron loaded')
 document.addEventListener("DOMContentLoaded", (event) => {
     log('Main Page loaded')
     loadPage('dashboard');
