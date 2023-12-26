f68d89b6dc080a4e34e61cc97e42b237a05a93cf
Spatulox <M78stormtrooper@laposte.net>
2023-12-26 23:51:32 +0100

    add retrieve theme from config.json in preload.mjs

--- preload.mjs
+++ preload.mjs
@@ -1,9 +1,13 @@
 import { loadPage } from "./src/modules/loadPages.js";
-import { log } from "./src/modules/globalFunction.js";
+import { log, readJsonFile } from "./src/modules/globalFunction.js";
 
 document.addEventListener("DOMContentLoaded", (event) => {
     const theme = document.getElementsByTagName('body')[0]
     const themeImg = document.getElementById('theme')
+
+    // Retrieve the saved theme inside the config.json
+    const savedTheme = readJsonFile("./config.json");
+    theme.classList = savedTheme.theme
 
     if(theme.classList == 'light'){
         themeImg.src = './src/images/black-moon.png'
