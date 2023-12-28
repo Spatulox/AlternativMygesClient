84b1279df8b17cc2ec21680f28d2729218e7d7ef
Spatulox <M78stormtrooper@laposte.net>
2023-12-28 22:48:17 +0100

    fix preload.msj : creating the config.json file

--- preload.mjs
+++ preload.mjs
@@ -1,31 +1,36 @@
 import { loadPage } from "./src/modules/loadPages.js";
-import { log, readJsonFile } from "./src/modules/globalFunction.js";
+import { log, readJsonFile, writeJsonFile, replaceValueJsonFile } from "./src/modules/globalFunction.js";
 
 document.addEventListener("DOMContentLoaded", (event) => {
-    // Check if there is a registered user
-    const user = readJsonFile("./src/data/infos.json");
     const config = readJsonFile("./config.json")
+    const user = readJsonFile("./src/data/infos.json");
 
-    if(!user){
-        log("Y'a pas d'utilisateur")
+    // Create the file is not exist
+    if(!config){
+        writeJsonFile(".", "config.json", {"theme":"","eula":""})
     }
 
+    // Check if there is a registered user
+    if(!user.login || !user.password){
+        log("No users registered, or can't acces to the data.")
+        const connection = document.getElementById('connection')
+        connection.classList.add('active')
+
+    }
+
+    // Check if the eula is accepted
     if(!config.eula || config.eula == "false")
     {
         const eula = document.getElementById('eula')
         eula.classList.add('active')
     }
 
-
-
-
-    // Check the rest of things like the saved theme
+    // Apply the theme
     const theme = document.getElementsByTagName('body')[0]
     const themeImg = document.getElementById('theme')
-
-    // Retrieve the saved theme inside the config.json
-    const savedTheme = readJsonFile("./config.json");
-    theme.classList = savedTheme.theme
+    if(config.theme){
+        theme.classList = config.theme
+    }
 
     if(theme.classList == 'light'){
         themeImg.src = './src/images/black-moon.png'
@@ -34,6 +39,8 @@ document.addEventListener("DOMContentLoaded", (event) => {
         themeImg.src = './src/images/black-sun.png'
     }
 
+
+    // Load the main page
     log('Main Page loaded')
     loadPage('dashboard');
 
