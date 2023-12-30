ba411fb991ba035d32d88d1d02d662882f5fd7cd
Spatulox <M78stormtrooper@laposte.net>
2023-12-30 15:06:13 +0100

    update schedule.js

--- src/js/schedule.js
+++ src/js/schedule.js
@@ -1,9 +1,32 @@
-import { log } from "../modules/globalFunction.js";
+import { log, readJsonFile, replaceValueJsonFile } from "../modules/globalFunction.js";
 import { popup, stillPopup, stopStillPopup } from "../modules/popup.js";
 import { checkXTimesInternetConnection } from "../modules/checkInternetCo.js";
 
 // Get schedule from myges website
+//Check if there is already a refreshing
+// This function exist to avoid to forget a replaceValueJsonFile when doing a retur inside the refreshingSchedule1()
 export async function refreshingSchedule(startDate = null, endDate = null){
+    
+    const tmp = readJsonFile('./config.json')
+
+    if(tmp.pendingSchedule == "true"){
+        log('There is already a refreching Schedule')
+        return
+    }
+
+    replaceValueJsonFile('./config.json', "pendingSchedule", "true")
+    try{
+        await refreshingSchedule1(startDate, endDate)
+    }
+    catch{
+        log('Error when refreshingSchedule1()')
+    }
+    
+    replaceValueJsonFile('./config.json', "pendingSchedule", "false")
+}
+
+// Refresh the Schedule
+async function refreshingSchedule1(startDate = null, endDate = null){
     stillPopup('Checking internet')
 
     //let tmp = 
@@ -21,10 +44,11 @@ export async function refreshingSchedule(startDate = null, endDate = null){
 
     // Write the file
 
+
+    stopStillPopup()
 }
 
 // Read the local file and print it inside the software in schedule page
 export function schedule(){
     refreshingSchedule()
-    console.log("Schedule function")
 }
