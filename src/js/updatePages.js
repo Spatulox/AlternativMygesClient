060301f69065a7d348385884f2e16e6f6edee1b1
Spatulox <M78stormtrooper@laposte.net>
2023-12-30 15:13:06 +0100

    new error message in updatePages.js

--- src/js/updatePages.js
+++ src/js/updatePages.js
@@ -8,12 +8,12 @@ import { grades } from "./grades.js";
 // Execute the function correspondig with the name
 export function updatePages(pages){
     const regex = /^[a-zA-Z_][a-zA-Z0-9_]*$/;
-    //try{
+    try{
         if (regex.test(pages) && pages !="softwareAccount" && pages !="credits") {
             eval(pages + '()');
         }
-    // }
-    // catch{
-    //     log(`Can't execute ${pages}() function, name is not valid`);
-    // }
+    }
+    catch(err){
+        log(`Can't execute ${pages}() function, name is not valid : ${err}`);
+    }
 }
