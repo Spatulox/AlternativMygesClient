58d7709cd00933f950c7114019a07d8d1608a16c
Spatulox <M78stormtrooper@laposte.net>
2023-12-30 01:43:49 +0100

    execute the function corresponfig to the name of the page loaded

--- src/js/updatePages.js
+++ src/js/updatePages.js
@@ -7,15 +7,12 @@ import { schedule } from "./schedule.js";
 // Execute the function correspondig with the name
 export function updatePages(pages){
     const regex = /^[a-zA-Z_][a-zA-Z0-9_]*$/;
-    
-    try{
-        if (regex.test(pages)) {
+    //try{
+        if (regex.test(pages) && pages !="softwareAccount" && pages !="credits") {
             eval(pages + '()');
-        } else {
-            log(`Can't execute ${pages}() function, name is not valid`);
         }
-    }
-    catch{
-        log(`Can't execute ${pages}() function, name is not valid`);
-    }
+    // }
+    // catch{
+    //     log(`Can't execute ${pages}() function, name is not valid`);
+    // }
 }
