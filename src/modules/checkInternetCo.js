6fa446035719429a1afe77458f68de4d6729d66b
Spatulox <M78stormtrooper@laposte.net>
2023-12-30 01:43:21 +0100

    add check connection functions

--- src/modules/checkInternetCo.js
+++ src/modules/checkInternetCo.js
@@ -1,53 +1,93 @@
-import isOnline from 'is-online';
-import { log } from './functions.js'
+const dns = require('dns');
+const { net } = require('electron');
 
 export async function checkInternetCo() {
-  let internetCo = 0;
+  const { log } = await import('./globalFunction.js')
 
-  while (internetCo === 0) {
-    const online = await isOnline();
-    log('CheckingInternet')
+  let internetCo = false;
 
-    if (online) {
-        log('Internet connection is available.');
-        internetCo = 1;
-    } else {
-        log('No internet connection, waiting 1 minute.');
-        try{
-          await new Promise((resolve) => setTimeout(resolve, 60000));
-        }
-        catch{
-          log('ERROR when awaiting the promise to await 1 minutes')
-        }
+  while (!internetCo) {
+    try {
+      await new Promise((resolve) => {
+        dns.lookup('google.com', (err) => {
+          if (err) {
+            log('No internet connection, waiting 1 minute.');
+            setTimeout(resolve, 60000);
+          } else {
+            log('Internet connection is available.');
+            internetCo = true;
+            resolve();
+          }
+        });
+      });
+    } catch (error) {
+      log('ERROR when awaiting the promise to await 1 minute');
     }
-
   }
 }
 
-
-export async function checkXTimesInternetCo(xTime) {
+/**/
+export async function checkXTimesInternetConnection(xTime = 10) {
+  // Don't touch ! NO if you add the log function, it gonna crash for unknown reason T_T
+  //const { log } = await import('./globalFunction.cjs')
   let count = 0;
 
-  while (count < xTime || count !== 'Connected') {
-    
-    log('CheckingInternet')
-    const online = await isOnline();
+  while (count < xTime) {
 
-    if (online) {
-        log('Internet connection is available.');
-        count = 'Connected';
-        return 'Connected'
-    } else {
-      count ++
-        log('No internet connection, waiting 1 minute.');
-        try{
-          await new Promise((resolve) => setTimeout(resolve, 60000));
+    let isConnected = await new Promise((resolve) => {
+      dns.lookup('google.com', (err) => {
+        if (err) {
+          resolve(false); // Pas de connexion Internet
+        } else {
+          resolve(true); // Connexion Internet disponible
         }
-        catch{
-          log('ERROR when awaiting the promise to await 1 minutes')
-        }
+      });
+    });
+
+    if (isConnected){
+      return true
     }
-
+    else {
+      count ++
+      // Don't touch !
+      console.log('No internet connection, waiting 1 minute.');
+      try{
+        await new Promise((resolve) => setTimeout(resolve, 60000));
+      }
+      catch{
+        // Don't touch !
+        console.log('ERROR when awaiting the promise to await 1 minutes')
+      }
+    }
   }
-  return 'Error'
 }
+/**/
+
+// export async function checkXTimesInternetConnection(xTime = 10) {
+//   let count = 0;
+
+//   while (count < xTime) {
+//     try {
+//       const request = net.request('https://www.google.com');
+//       request.on('response', () => {
+//         log('Internet connection is available.');
+//         // Mettre en œuvre la logique supplémentaire ici
+//       });
+//       request.on('error', () => {
+//         count++;
+//         log('No internet connection, waiting 1 minute.');
+//         if (count >= xTime) {
+//           // Mettre en œuvre la logique supplémentaire ici
+//         } else {
+//           setTimeout(() => {
+//             request.end();
+//           }, 60000);
+//         }
+//       });
+//       request.end();
+//     } catch (error) {
+//       log('ERROR when awaiting the promise to await 1 minute');
+//       // Gérer l'erreur
+//     }
+//   }
+// }
