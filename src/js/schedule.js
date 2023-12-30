b42ad6793ef76a5e5fe31a9612f8eba2122b9259
Spatulox <M78stormtrooper@laposte.net>
2023-12-30 01:45:07 +0100

    begin refreshing schedule

--- src/js/schedule.js
+++ src/js/schedule.js
@@ -1,5 +1,30 @@
 import { log } from "../modules/globalFunction.js";
+import { popup, stillPopup, stopStillPopup } from "../modules/popup.js";
+import { checkXTimesInternetConnection } from "../modules/checkInternetCo.js";
 
+// Get schedule from myges website
+export async function refreshingSchedule(startDate = null, endDate = null){
+    stillPopup('Checking internet')
+
+    //let tmp = 
+    if(!(await checkXTimesInternetConnection(10))){
+        log('Definitely no Internet connection')
+        popup('No internet connection')
+        stopStillPopup()
+    }
+    else{
+        //popup('Internet co !')
+        stillPopup('Connecting to myGes api')
+    }
+
+
+
+    // Write the file
+
+}
+
+// Read the local file and print it inside the software in schedule page
 export function schedule(){
+    refreshingSchedule()
     console.log("Schedule function")
 }
